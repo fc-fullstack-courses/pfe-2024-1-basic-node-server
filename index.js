978b684b26443b30dbc32afc3b23f73baf6ae46d
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-20 21:12:16 +0200

    Add basic node server logic for POST and GET routes

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 const http = require('http');
 
+const users = [{ id: 0 }, { id: 1 }, { id: 2 }];
+
 // функція, що зпускається при надхоженні запиту на сервер
-function requestListener (request, response) {
-  console.log(request); // об'єкт запита клієнта
-  console.log(response); // об'єкт відповіді сервера
+function requestListener(request, response) {
+  // console.log(request); // об'єкт запита клієнта
+  // console.log(response); // об'єкт відповіді сервера
+
+  // рядок методу з запиту
+  const method = request.method;
+  // рядок з шляхом на який було зроблшено запит у сервері (без хоста і порта)
+  const path = request.url;
+
+  if (method === 'GET') {
+    if (path === '/users') {
+      response.end(JSON.stringify(users));
+    } else if (path === '/comments') {
+      response.end(JSON.stringify([{ id: 0, text: 'bla bla bla' }]));
+    }
+  } else if (method === 'POST') {
+
+    if(path === '/users') {
+      let userJsonString = '';
+
+      request.on('data', (chunk) => {
+        userJsonString += chunk;
+      });
+
+      request.on('end', () => {
+        const user = JSON.parse(userJsonString);
+
+        // ...
+
+        user.id = Date.now();
+
+        users.push(user);
+
+        response.end('User registered');
+      });
+    }
+  }
 
   // посилаємо відповідь кліенту який робив запит
   // має бути рядком і має бути обов'язково присутнім
-  response.end('Thanks for visiting!');
+  // response.end(`Thanks for visiting! Your method was ${request.method}`);
 }
 
 // створюємо екземпляр сервера
