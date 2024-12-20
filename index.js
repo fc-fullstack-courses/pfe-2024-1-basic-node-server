ca3924d73a04b78e24fb3df2b6c718cc198de6ae
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-20 18:50:15 +0200

    Add CommonJS syntax examples

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // імпорт за замовчанням (ESModules)
-import MyMathModule from './myMath.js';
+// import MyMathModule from './myMath.js';
+
+// імпорт за замовчанням (CommonJS)
+const myMathExports = require('./myMath.js');
+
+console.log(myMathExports);
 
 // іменний імпорт (ESModules)
-import { test, testFunc as newTestFuncName } from './myMath.js';
+// import { test, testFunc as newTestFuncName } from './myMath.js';
 
-import * as MyMath from './myMath.js';
+// import * as MyMath from './myMath.js';
 
-console.log(MyMath);
+// іменний імпорт (CommonJS)
+const { test, testFunc: myTestFunc } = require('./myMath.js');
 
-newTestFuncName();
+// console.log(MyMath);
+
+// newTestFuncName();
+
+// console.log(__dirname); // шлях до папки що містить файл
+// console.log(__filename); // шлях до файл що запущено
+
+// console.log(module);
--- myMath.js
+++ myMath.js
@@ -3,16 +3,41 @@ class MyMath {
 }
 
 // експорт за замовчанням (ESModules)
-export default MyMath;
+// export default MyMath;
 
 // export default 5; ERROR
 
+// експорт за замовчанням (CommonJS)
+
+module.exports = MyMath;
+
+// module.exports =  5; // не помилка але перезапис
+
 // іменний експорт (ESModules)
-export { MyMath };
+// export { MyMath };
 
-export const test = 5;
+// export const test = 5;
 
-export function testFunc () {
+// export function testFunc () {
+//   console.log('test func');
+// }
+
+// іменний експорт (CommonJS)
+
+// якщо exports це об'єкт
+module.exports.test = 5;
+
+module.exports.testFunc = () => {
   console.log('test func');
+};
+
+// якщо exports це не об'єкт
+module.exports = {
+  test: 5,
+  MyMath,
+  testFunc : () => {
+    console.log('test func');
+  }
 }
 
+// console.log(module);
